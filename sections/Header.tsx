1cb648ad6ab61e53ed0e8443b0ae332420ed66de
Shahzaib Javaid <shahzaibjavaid68@gmail.com>
2025-01-21 20:07:30 +0500

    Mobile nav completed

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,3 +1,5 @@
+import logoImage from '@/assets/images/sphereal-logo.svg?url'
+
 export const navItems = [
   {
     name: 'Features',
@@ -27,7 +29,35 @@ export const loginItems = [
 ]
 
 export const Header = () => {
-  return <header>header section</header>
+  return (
+    <header className='border-b border-gray-200/20'>
+      <div className='container'>
+        <div className='h-18 flex justify-between items-center'>
+          <div className='flex gap-4 items-center'>
+            <div
+              className='size-10 bg-gray-200 bg-[conic-gradient(from_45deg,var(--color-violet-400),var(--color-fuchsia-400),var(--color-amber-300),var(--color-teal-300),var(--color-violet-400))]'
+              style={{
+                maskImage: `url(${logoImage.src})`,
+                maskSize: 'contain',
+              }}
+            ></div>
+            <div className='font-extrabold text-2xl'>sphereal.ai</div>
+          </div>
+          <div></div>
+          <div className='flex items-center'>
+            <button className='size-10 rounded-lg border-2 border-transparent [background:linear-gradient(var(--color-gray-950),var(--color-gray-950))_content-box,conic-gradient(from_45deg,var(--color-violet-400),var(--color-fuchsia-400),var(--color-amber-300),var(--color-teal-300),var(--color-violet-400))_border-box] relative'>
+              <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                <div className='w-4 h-0.5 bg-gray-100 -translate-y-1'></div>
+              </div>
+              <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                <div className='w-4 h-0.5 bg-gray-100 translate-y-1'></div>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
